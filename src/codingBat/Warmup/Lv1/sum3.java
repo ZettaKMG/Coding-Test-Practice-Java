e352056de2bb26137f2b36e4c48a7c1482a313b1
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-09-28 22:30:19 +0900

    day 1

--- src/codingBat/Warmup/Lv1/sum3.java
+++ src/codingBat/Warmup/Lv1/sum3.java
@@ -4,7 +4,15 @@ public class sum3 {
 	
 	public int sum3(int[] nums) {		
 		
-		return nums[0] + nums[1] + nums[2];
+//		return nums[0] + nums[1] + nums[2];
+		
+		int sum = 0;
+		
+		for (int i = 0; i <= nums.length-1; i++) {
+			sum += nums[i];
+		}
+		
+		return sum;
 		
 	}
 }
