baffb4e622fcdd081e5704362f5a1a2e6e3f8001
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-09-29 15:35:52 +0900

    day2

--- src/codingBat/Logic/Lv1/cigarParty.java
+++ src/codingBat/Logic/Lv1/cigarParty.java
@@ -1,5 +1,13 @@
 package codingBat.Logic.Lv1;
 
 public class cigarParty {
-
+	public boolean cigarParty(int cigars, boolean isWeekend) {
+		
+		if (isWeekend) {
+			return cigars >= 40;
+		} else {
+			return cigars >= 40 && cigars <= 60;
+		}
+		
+	}	
 }
--- src/codingBat/Logic/Lv1/dateFashion.java
+++ src/codingBat/Logic/Lv1/dateFashion.java
@@ -1,5 +1,19 @@
 package codingBat.Logic.Lv1;
 
 public class dateFashion {
-
+	public int dateFashion (int you, int date) {
+		
+		int result = 1;
+		
+		if (you >= 8 || date >= 8) {
+			result = 2;
+		}
+		
+		if (you <= 2 || date <=2 ) {
+			result = 0;
+		}
+		
+		return result;
+		
+	}
 }
