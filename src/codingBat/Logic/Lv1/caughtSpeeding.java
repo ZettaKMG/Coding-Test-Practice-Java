78ace19dccd9616c87eee3e58fc7afb9c6692737
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-09-30 15:49:46 +0900

    day3

--- src/codingBat/Logic/Lv1/caughtSpeeding.java
+++ src/codingBat/Logic/Lv1/caughtSpeeding.java
@@ -1,5 +1,23 @@
 package codingBat.Logic.Lv1;
 
 public class caughtSpeeding {
-
+	public int caughtSpeeding(int speed, boolean isBirthday) {
+		
+		int limit1 = 61;
+		int limit2 = 81;
+		
+		if (isBirthday) {
+			limit1 += 5;
+			limit2 += 5;
+		}
+		
+		if (speed >= limit2) {
+			return 2;
+		} else if (speed >= limit1 && speed < limit2) {
+			return 1;
+		} else {
+			return 0;
+		}
+		
+	}
 }
