d9a5a76e9ec95f357c9ea69df2f2483c49ed6fa1
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-09-30 15:34:38 +0900

    day3

--- src/codingBat/Logic/Lv1/squirrelPlay.java
+++ src/codingBat/Logic/Lv1/squirrelPlay.java
@@ -1,5 +1,13 @@
 package codingBat.Logic.Lv1;
 
 public class squirrelPlay {
-
+	public boolean squirrelPlay (int temp, boolean isSummer) {
+		
+		if (isSummer) {
+			return temp >= 60 && temp <= 100;
+		} else {
+			return temp >= 60 && temp <= 90;
+		}
+		
+	}
 }
