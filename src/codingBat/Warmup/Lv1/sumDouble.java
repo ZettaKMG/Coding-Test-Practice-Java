d8072023c586dc8c04c7bd908117f6ec7118b777
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-01 11:28:21 +0900

    day4

---
+++ src/codingBat/Warmup/Lv1/sumDouble.java
@@ -0,0 +1,15 @@
+package codingBat.Warmup.Lv1;
+
+public class sumDouble {
+	public int sumDouble(int a, int b) {
+		
+//		return a == b ? 2 * (a + b) : a + b;
+		
+		if (a != b) {
+			return a + b;
+		} else {
+			return 2 * (a + b);
+		}
+		
+	}
+}
