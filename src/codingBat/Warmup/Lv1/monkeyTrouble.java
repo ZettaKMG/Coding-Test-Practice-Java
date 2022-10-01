27dbc288571ec1f27036818f509f3b102d2b76f9
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-01 11:23:40 +0900

    day4

---
+++ src/codingBat/Warmup/Lv1/monkeyTrouble.java
@@ -0,0 +1,17 @@
+package codingBat.Warmup.Lv1;
+
+public class monkeyTrouble {
+	public boolean monkeyTrouble (boolean aSmile, boolean bSmile) {
+		
+//		return (aSmile && bSmile) || (!aSmile && !bSmile);
+		
+		if (aSmile && bSmile) {
+			return true;
+		} else if (!aSmile && !bSmile) {
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+}
