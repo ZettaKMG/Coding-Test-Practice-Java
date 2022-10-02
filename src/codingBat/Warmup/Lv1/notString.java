ab47affbd0102448bffed5f3411c1ebb5f4d4070
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-02 12:47:44 +0900

    day5

---
+++ src/codingBat/Warmup/Lv1/notString.java
@@ -0,0 +1,19 @@
+package codingBat.Warmup.Lv1;
+
+public class notString {
+	public String notString(String str) {
+		
+		String FrontWord = "";
+		
+		if (str.length() >= 3) {
+			FrontWord = str.substring(0, 3);
+		}
+		
+		if (FrontWord.equals("not")) {
+			return str;
+		} else {
+			return "not " + str;
+		}
+		
+	}
+}
