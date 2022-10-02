2681c35f5e33bcfe1bae38277fde5c01ee12a323
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-02 12:19:29 +0900

    day5

---
+++ src/codingBat/Warmup/Lv1/makes10.java
@@ -0,0 +1,15 @@
+package codingBat.Warmup.Lv1;
+
+public class makes10 {
+	public boolean makes10(int a, int b) {
+		
+		if (a == 10 || b == 10) {
+			return true;
+		} else if (a + b == 10) {
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+}
