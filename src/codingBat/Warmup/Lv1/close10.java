62723127cad63cd9f7ea1458399a05b2091be3d4
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-03 14:47:26 +0900

    day6

--- src/codingBat/Warmup/Lv1/close10.java
+++ src/codingBat/Warmup/Lv1/close10.java
@@ -1,5 +1,13 @@
 package codingBat.Warmup.Lv1;
 
 public class close10 {
-
+	public int close10(int a, int b) {
+		
+		if (Math.abs(10 - a) == Math.abs(10 - b)) {
+			return 0;
+		} else {
+			return Math.abs(10 - b) > Math.abs(10 - a) ? a : b;
+		}
+		
+	}
 }
