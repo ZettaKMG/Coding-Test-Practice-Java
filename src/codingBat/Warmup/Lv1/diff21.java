5cb09e14e2cf6e9cc70f18c74ad6629063ec38d0
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-03 14:39:03 +0900

    day6

--- src/codingBat/Warmup/Lv1/diff21.java
+++ src/codingBat/Warmup/Lv1/diff21.java
@@ -2,6 +2,12 @@ package codingBat.Warmup.Lv1;
 
 public class diff21 {
 	public int diff21(int n) {
+
+		if (n > 21) {
+			return 2 * Math.abs(n - 21);
+		} else {
+			return Math.abs(n - 21);
+		}
 		
 	}
 }
