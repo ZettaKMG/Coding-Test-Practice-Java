c8cff459f340f9a6858a2ccb366de867159a5c2c
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-03 14:54:44 +0900

    day6

--- src/codingBat/String/Lv1/firstTwo.java
+++ src/codingBat/String/Lv1/firstTwo.java
@@ -1,5 +1,13 @@
 package codingBat.String.Lv1;
 
 public class firstTwo {
-
+	public String firstTwo(String str) {
+		
+		if (str.length() >= 2) {
+			return str.substring(0, 2);
+		} else {
+			return str;
+		}
+		
+	}
 }
