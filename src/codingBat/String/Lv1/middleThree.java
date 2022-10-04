e8653649a78adaa6f52d61781c165138946ee3dd
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-04 14:51:30 +0900

    day7

--- src/codingBat/String/Lv1/middleThree.java
+++ src/codingBat/String/Lv1/middleThree.java
@@ -1,5 +1,11 @@
 package codingBat.String.Lv1;
 
 public class middleThree {
-
+	public String middleThree(String str) {
+		
+		int n = str.length() - 1;
+		
+		return str.substring(n / 2 - 1, n / 2 + 2);
+		
+	}
 }
