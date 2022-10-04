cff146802ca7ccecc6f490d041f4157317fa382e
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-04 16:21:28 +0900

    day7

--- src/codingBat/String/Lv1/seeColor.java
+++ src/codingBat/String/Lv1/seeColor.java
@@ -1,5 +1,15 @@
 package codingBat.String.Lv1;
 
 public class seeColor {
-
+	public String seeColor(String str) {
+		
+		if (str.indexOf("red") == 0) {
+			return "red";
+		} else if (str.indexOf("blue") == 0) {
+			return "blue";
+		} else {
+			return "";
+		}
+		
+	}
 }
