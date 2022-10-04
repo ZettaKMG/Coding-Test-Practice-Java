8c31cca0b51a38002b7c5f9314880e60bfe8a8b9
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-04 16:01:04 +0900

    day7

--- src/codingBat/String/Lv1/frontAgain.java
+++ src/codingBat/String/Lv1/frontAgain.java
@@ -1,5 +1,16 @@
 package codingBat.String.Lv1;
 
 public class frontAgain {
-
+	public boolean frontAgain(String str) {
+		
+		if (str.length() < 2) {
+			return false;
+		}
+		
+		String end = str.substring(str.length() - 2);
+		int i = str.indexOf(end);
+		
+		return i == 0;
+		
+	}
 }
