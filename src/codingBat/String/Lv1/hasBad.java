7ebd06a642f1b1d6ae6f65c8695d25786a32a218
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-04 15:19:39 +0900

    day7

--- src/codingBat/String/Lv1/hasBad.java
+++ src/codingBat/String/Lv1/hasBad.java
@@ -1,5 +1,13 @@
 package codingBat.String.Lv1;
 
 public class hasBad {
-
+	public boolean hasBad(String str) {
+	
+		if (str.indexOf("bad") == 0 || str.indexOf("bad") == 1) {
+			return true;
+		} else {
+			return false;
+		}
+		
+	}	
 }
