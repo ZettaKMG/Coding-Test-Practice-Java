f5177f9825badf1ec342edec5747bca5f41a9651
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-06 14:47:23 +0900

    day9

--- src/codingBat/Map/Lv1/topping2.java
+++ src/codingBat/Map/Lv1/topping2.java
@@ -1,9 +1,19 @@
 package codingBat.Map.Lv1;
 
+import java.util.Map;
+
 public class topping2 {
 	public Map<String, String> topping2(Map<String, String> map) {
 		
+		if (map.containsKey("ice cream")) {
+			map.put("yogurt", map.get("ice cream"));
+		}
 		
+		if (map.containsKey("spinach")) {
+			map.put("spinach", "nuts");
+		}
+		
+		return map;
 		
 	}
 }
