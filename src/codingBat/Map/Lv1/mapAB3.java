ba041f08cf7a726c2599e33e6836e860165c88f4
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-06 15:37:14 +0900

    day9

--- src/codingBat/Map/Lv1/mapAB3.java
+++ src/codingBat/Map/Lv1/mapAB3.java
@@ -1,5 +1,25 @@
 package codingBat.Map.Lv1;
 
-public class mapAB3 {
+import java.util.Map;
 
+public class mapAB3 {
+	public Map<String, String> mapAB3(Map<String, String> map) {
+		
+		boolean has_a = map.containsKey("a");
+		boolean has_b = map.containsKey("b");
+		
+		String a = map.get("a");
+		String b = map.get("b");
+		
+		if (has_a && has_b) {
+			return map;
+		} else if (has_a) {
+			map.put("b", a);
+		} else if (has_b) {
+			map.put("a", b);
+		}
+		
+		return map;
+		
+	}
 }
