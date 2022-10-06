8d7040223e427348e2e13f7633e90a70c8a33561
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-06 15:14:35 +0900

    day9

--- src/codingBat/Map/Lv1/mapAB2.java
+++ src/codingBat/Map/Lv1/mapAB2.java
@@ -1,5 +1,19 @@
 package codingBat.Map.Lv1;
 
-public class mapAB2 {
+import java.util.Map;
 
+public class mapAB2 {
+	public Map<String, String> mapAB2(Map<String, String> map) {
+		
+		String a = map.get("a");
+		String b = map.get("b");
+		
+		if (a != null && a.equals(b)) {
+			map.remove("a");
+			map.remove("b");
+		}
+		
+		return map;
+		
+	}
 }
