3d4f5896ac57041b7977cf17edb5a456bed88042
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-09 07:24:17 +0900

    day12

--- src/codingBat/Functional/Lv1/lower.java
+++ src/codingBat/Functional/Lv1/lower.java
@@ -1,5 +1,13 @@
 package codingBat.Functional.Lv1;
 
-public class lower {
+import java.util.List;
 
+public class lower {
+	public List<String> lower(List<String> strings) {
+		
+		strings.replaceAll(s -> s.toLowerCase());
+		
+		return strings;
+		
+	}
 }
