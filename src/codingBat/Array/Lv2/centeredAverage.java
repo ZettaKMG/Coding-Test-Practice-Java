9c19a80ebc0a8efd1a414043a1db20fca32044e3
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-09 08:21:21 +0900

    day12

--- src/codingBat/Array/Lv2/centeredAverage.java
+++ src/codingBat/Array/Lv2/centeredAverage.java
@@ -1,5 +1,25 @@
 package codingBat.Array.Lv2;
 
 public class centeredAverage {
-
+	public int centerAverage(int[] nums) {
+		
+		int total = 0;
+		int max = nums[0];
+		int min = nums[0];
+		
+		for (int i = 0; i < nums.length; i++) {
+			if (max < nums[i]) {
+				max = nums[i];				
+			}
+			
+			if (min > nums[i]) {
+				min = nums[i];
+			}
+			
+			total += nums[i];
+		}
+		
+		return (total - max - min) / (nums.length - 2);
+		
+	}
 }
