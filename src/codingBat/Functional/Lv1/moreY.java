e20b1f0835f6fcbf62fd81be88c7c2d2b18f92a0
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-08 09:05:46 +0900

    day11

--- src/codingBat/Functional/Lv1/moreY.java
+++ src/codingBat/Functional/Lv1/moreY.java
@@ -1,5 +1,13 @@
 package codingBat.Functional.Lv1;
 
-public class moreY {
+import java.util.List;
 
+public class moreY {
+	public List<String> moreY(List<String> strings) {
+		
+		strings.replaceAll(s -> "y" + s + "y");
+		
+		return strings;
+		
+	}
 }
