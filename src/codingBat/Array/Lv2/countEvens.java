fb95a847bc735affcde0c14fb017fce06e0bb575
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-09 08:11:31 +0900

    day12

--- src/codingBat/Array/Lv2/countEvens.java
+++ src/codingBat/Array/Lv2/countEvens.java
@@ -1,5 +1,17 @@
 package codingBat.Array.Lv2;
 
 public class countEvens {
-
+	public int countEvens(int[] nums) {
+		
+		int number_of_even = 0;
+		
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] % 2 == 0) {
+				number_of_even++;
+			}
+		}
+		
+		return number_of_even;
+		
+	}
 }
