a774c0b923ad1a0c18a1ad08f46dac2211a1c1a2
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-08 09:03:23 +0900

    day11

--- src/codingBat/Functional/Lv1/copies3.java
+++ src/codingBat/Functional/Lv1/copies3.java
@@ -1,5 +1,11 @@
 package codingBat.Functional.Lv1;
 
 public class copies3 {
-
+	public List<String> copies3(List<String> strings) {
+		
+		strings.replaceAll(s -> s + s + s);
+		
+		return strings;
+		
+	}	
 }
