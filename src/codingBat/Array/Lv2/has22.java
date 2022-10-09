441a41022e061d25d6ad9cc0ff7179ad92d0b168
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-10 07:50:00 +0900

    day13

--- src/codingBat/Array/Lv2/has22.java
+++ src/codingBat/Array/Lv2/has22.java
@@ -1,5 +1,22 @@
 package codingBat.Array.Lv2;
 
 public class has22 {
-
+	public boolean has22(int[] nums) {
+		
+		int i = 0;
+		
+		while (i < nums.length - 1) {
+			
+			if (nums[i] == 2) {
+				if (nums[i + 1] == 2) {
+					return true;
+				}
+			}
+			
+			i++;
+		}
+		
+		return false;
+		
+	}
 }
