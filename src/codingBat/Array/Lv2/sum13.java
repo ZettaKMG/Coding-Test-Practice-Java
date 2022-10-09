914fe36c4e83759047a6c65eb1e5e123fbd73e5e
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-10 07:04:08 +0900

    day13

--- src/codingBat/Array/Lv2/sum13.java
+++ src/codingBat/Array/Lv2/sum13.java
@@ -1,5 +1,25 @@
 package codingBat.Array.Lv2;
 
 public class sum13 {
-
+	public int sum13(int[] nums) {
+		
+		int total = 0;
+		boolean is13 = 0;
+		boolean was13 = 0;
+		
+		for (int i = 0; i < nums.length; i++) {
+			
+			is13 = nums[i] == 13;
+			
+			if (!was13 && !is13) {
+				total += nums[i];
+			}
+			
+			was13 = is13;
+			
+		}
+		
+		return total;		
+		
+	}
 }
