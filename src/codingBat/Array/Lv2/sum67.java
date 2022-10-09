352873ed0913aab75be513551573f3603a4f0b79
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-10 07:14:19 +0900

    day13

--- src/codingBat/Array/Lv2/sum67.java
+++ src/codingBat/Array/Lv2/sum67.java
@@ -1,5 +1,27 @@
 package codingBat.Array.Lv2;
 
 public class sum67 {
-
+	public int sum67(int[] nums) {
+		
+		int total = 0;
+		
+		int i = 0;
+		
+		while (i < nums.length) {
+			
+			if (nums[i] == 6) {
+				while (nums[i] != 7) {
+					i++;
+				}
+			} else {
+				total += nums[i];
+			}
+			
+			i++;
+			
+		}
+				
+		return total;
+		
+	}
 }
