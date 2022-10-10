204e96038359d61801bc41cf9f32710c049f0af9
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-10 09:17:53 +0900

    day13

--- src/codingBat/Array/Lv2/lucky13.java
+++ src/codingBat/Array/Lv2/lucky13.java
@@ -1,5 +1,21 @@
 package codingBat.Array.Lv2;
 
 public class lucky13 {
-
+	public boolean lucky13(int[] nums) {
+		
+		int i = 0;
+		
+		while (i < nums.length) {
+			
+			if (nums[i] == 1 || nums[i] == 3) {
+				return false;
+			}
+			
+			i++;
+			
+		}
+		
+		return true;
+		
+	}
 }
