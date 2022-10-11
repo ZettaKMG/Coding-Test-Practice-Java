6379755f28bad068a8b31ba270f825379917123a
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-11 11:15:03 +0900

    day14

--- src/codingBat/Array/Lv2/fizzArray3.java
+++ src/codingBat/Array/Lv2/fizzArray3.java
@@ -1,5 +1,15 @@
 package codingBat.Array.Lv2;
 
 public class fizzArray3 {
-
+	public int[] fizzArray3(int start, int end) {
+		
+		int[] result = new int[end - start];
+		
+		for (int i = start; i < end; i++) {
+			result[i - start] = i;
+		}
+		
+		return result;
+		
+	}
 }
