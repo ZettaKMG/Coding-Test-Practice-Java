9ba05b7531cdb9f177c1acbe246b62a2c97d774e
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-11 11:21:51 +0900

    day14

--- src/codingBat/Array/Lv2/shiftLeft.java
+++ src/codingBat/Array/Lv2/shiftLeft.java
@@ -1,5 +1,15 @@
 package codingBat.Array.Lv2;
 
 public class shiftLeft {
-
+	public int[] shiftLeft(int[] nums) {
+		
+		int[] result = new int[nums.length];
+		
+		for (int i = 0; i < nums.length; i++) {
+			result[i] = nums[(i + 1) % nums.length];
+		}
+		
+		return result;
+		
+	}
 }
