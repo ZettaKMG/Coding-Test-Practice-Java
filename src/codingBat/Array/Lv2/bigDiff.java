d606010141678fdd85bde4aa43a89a1b6933807f
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-11 11:40:39 +0900

    day14

--- src/codingBat/Array/Lv2/bigDiff.java
+++ src/codingBat/Array/Lv2/bigDiff.java
@@ -1,5 +1,29 @@
 package codingBat.Array.Lv2;
 
 public class bigDiff {
-
+	public int bigDiff(int[] nums) {
+		
+		int max = nums[0];
+		int min = nums[0];
+		
+		for (int i = 0; i < nums.length; i++) {
+			
+			if (max < nums[i]) {
+				max = nums[i];
+			}
+			
+		}
+		
+		for (int j = 0; j < nums.length; j++) {
+			
+			if (min > nums[j]) {
+				min = nums[j];
+			}
+			
+		}
+		
+		return max - min;
+			
+		
+	}
 }
