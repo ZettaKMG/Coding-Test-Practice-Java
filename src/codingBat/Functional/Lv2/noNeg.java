34c8f063b7a8a3b9d8cf767b0ee33ece323571c1
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-12 22:35:44 +0900

    day15

--- src/codingBat/Functional/Lv2/noNeg.java
+++ src/codingBat/Functional/Lv2/noNeg.java
@@ -1,5 +1,13 @@
 package codingBat.Functional.Lv2;
 
+import java.util.List;
+
 public class noNeg {
-	
+	public List<Integer> noNeg(List<Integer> nums) {
+		
+		nums.removeIf(n -> n < 0);
+		
+		return nums;
+		
+	}
 }
