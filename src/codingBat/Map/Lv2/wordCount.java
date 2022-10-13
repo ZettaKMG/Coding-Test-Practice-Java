3ac25cd2973dd210e9517d496b3354ca33010e7b
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-14 07:17:13 +0900

    day17

--- src/codingBat/Map/Lv2/wordCount.java
+++ src/codingBat/Map/Lv2/wordCount.java
@@ -1,5 +1,25 @@
 package codingBat.Map.Lv2;
 
-public class wordCount {
+import java.util.HashMap;
+import java.util.Map;
 
+public class wordCount {
+	public Map<String, Integer> wordCount(String[] strings) {
+	
+		Map<String, Integer> map = new HashMap<>();
+		
+		for (String str : strings) {
+			
+			if (map.containsKey(str)) {
+				int value = map.get(str);
+				map.put(str, value + 1);
+			} else {
+				map.put(str, 1);
+			}
+			
+		}
+		
+		return map;
+		
+	}	
 }
