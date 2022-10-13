88257f1d6b2ef7532bd1e70b2e4fae7cdd672e11
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-14 07:11:30 +0900

    day17

--- src/codingBat/Map/Lv2/wordAppend.java
+++ src/codingBat/Map/Lv2/wordAppend.java
@@ -1,5 +1,32 @@
 package codingBat.Map.Lv2;
 
-public class wordAppend {
+import java.util.HashMap;
+import java.util.Map;
 
+public class wordAppend {
+	public String wordAppend(String[] strings) {
+		
+		String result = "";
+		Map<String, Integer> map = new HashMap<>();
+		
+		for (String str : strings) {
+			String key = str;
+			
+			if (map.containsKey(key)) {
+				Integer prev = map.get(key);
+				map.put(key, prev + 1);
+			} else {
+				map.put(key, 1);
+			}
+			
+			Integer cur = map.get(key);
+			
+			if (cur % 2 == 0) {
+				result += key;
+			}
+		}
+		
+		return result;
+		
+	}
 }
