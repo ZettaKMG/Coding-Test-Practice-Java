042c78b42a9b98ee3c9c423ac3642dca9cc95461
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-14 07:14:24 +0900

    day17

--- src/codingBat/Map/Lv2/firstChar.java
+++ src/codingBat/Map/Lv2/firstChar.java
@@ -1,5 +1,25 @@
 package codingBat.Map.Lv2;
 
-public class firstChar {
+import java.util.HashMap;
+import java.util.Map;
 
+public class firstChar {
+	public Map<String, String> firstChar(String[] strings) {
+		
+		Map<String, String> map = new HashMap<>();
+		
+		for (String str : strings) {
+			String key = str.substring(0, 1);
+			
+			if (map.containsKey(key)) {
+				String prev = map.get(key);
+				map.put(key, prev + str);
+			} else {
+				map.put(key, str);
+			}
+		}
+		
+		return map;
+		
+	}
 }
