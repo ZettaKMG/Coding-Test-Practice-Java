6b3774e0587500a0919761607a173e2b13d51292
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-14 07:18:33 +0900

    day17

--- src/codingBat/Map/Lv2/pairs.java
+++ src/codingBat/Map/Lv2/pairs.java
@@ -1,9 +1,23 @@
 package codingBat.Map.Lv2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class pairs {
 	public Map<String, String> pairs(String[] strings) {
 		
+		Map<String, String> map = new HashMap<>();
 		
+		for (String str : strings) {
+			
+			String key = str.substring(0, 1);
+			String value = str.substring(str.length() - 1);
+			
+			map.put(key, value);	
+			
+		}
+		
+		return map;
 		
 	}
 }
