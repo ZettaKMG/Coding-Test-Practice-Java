9a5b55c796b414d4f0e10e963a344aab9a859a38
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-15 09:38:32 +0900

    day18

--- src/codingBat/Map/Lv2/wordMultiple.java
+++ src/codingBat/Map/Lv2/wordMultiple.java
@@ -1,5 +1,20 @@
 package codingBat.Map.Lv2;
 
-public class wordMultiple {
+import java.util.HashMap;
+import java.util.Map;
 
+public class wordMultiple {
+	public Map<String, Boolean> wordMultiple(String[] strings) {
+		
+		Map<String, Boolean> map = new HashMap<>();
+		
+		for (String str : strings) {
+			
+			map.put(str, map.containsKey(str));
+			
+		}
+		
+		return map;
+		
+	}
 }
