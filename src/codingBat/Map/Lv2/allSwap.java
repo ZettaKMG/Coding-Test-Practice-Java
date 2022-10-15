68f7febfd74f44b304ec9a88bfe18fb5ecaa04e5
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-15 12:53:09 +0900

    day18

--- src/codingBat/Map/Lv2/allSwap.java
+++ src/codingBat/Map/Lv2/allSwap.java
@@ -1,5 +1,32 @@
 package codingBat.Map.Lv2;
 
-public class allSwap {
+import java.util.HashMap;
+import java.util.Map;
 
+public class allSwap {
+	public String[] allSwap(String[] strings) {
+		
+		Map<Character, Integer> map = new HashMap<>();
+		
+		for (int i = 0; i < strings.length; i++) {
+			
+			String str = strings[i];
+			Character key = str.charAt(0);
+			
+			if (map.containsKey(key)) {
+				Integer prev_index = map.get(key);
+				
+				String temp = strings[prev_index];
+				strings[prev_index] = strings[i];
+				strings[i] = temp;
+				
+				map.remove(key);				
+			} else {
+				map.put(key, i);
+			}
+		}
+		
+		return strings;
+		
+	}
 }
