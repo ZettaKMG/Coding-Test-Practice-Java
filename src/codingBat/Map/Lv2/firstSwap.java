1b90184e19142f0073703f7fcbd38736b328728a
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-15 13:04:21 +0900

    day18

--- src/codingBat/Map/Lv2/firstSwap.java
+++ src/codingBat/Map/Lv2/firstSwap.java
@@ -1,5 +1,34 @@
 package codingBat.Map.Lv2;
 
-public class firstSwap {
+import java.util.HashMap;
+import java.util.Map;
 
+public class firstSwap {
+	public String[] firstSwap(String[] strings) {
+		
+		Map<Character, Integer> map = new HashMap<>();
+		
+		for (int i =0; i < strings.length; i++) {
+			
+			String str = strings[i];
+			Character key = str.charAt(0);
+								
+			if (map.containsKey(key)) {
+				Integer prev_index = map.get(key);
+				
+				if (prev_index != -1) {
+					String temp = strings[prev_index];
+					strings[prev_index] = strings[i];
+					strings[i] = temp;
+					
+					map.put(key, -1);
+				}				
+			} else {
+				map.put(key, i);
+			}
+		}
+		
+		return strings;
+		
+	}
 }
