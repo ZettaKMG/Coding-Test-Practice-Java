297e9f7ab705bd49084a2f09003f6a706483fe22
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-16 23:10:23 +0900

    day19

--- src/CodingBat/String/Lv2/endOther.java
+++ src/CodingBat/String/Lv2/endOther.java
@@ -1,5 +1,16 @@
 package CodingBat.String.Lv2;
 
 public class endOther {
-
+	public boolean endOther(String a, String b) {
+				
+		String a_low = a.toLowerCase();
+		String b_low = b.toLowerCase();
+		
+		if (a_low.endsWith(b_low) || b_low.endsWith(a_low)) {
+			return true;
+		}
+		
+		return false;
+		
+	}
 }
