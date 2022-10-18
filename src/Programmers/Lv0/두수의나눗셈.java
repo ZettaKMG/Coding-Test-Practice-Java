023ec9b0a5ba3fd2c8a0cc274d4b293ba62f45f6
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-18 12:30:49 +0900

    day21(JavaScript)

--- src/Programmers/Lv0/두수의나눗셈.java
+++ src/Programmers/Lv0/두수의나눗셈.java
@@ -1,5 +1,10 @@
 package Programmers.Lv0;
 
 public class 두수의나눗셈 {
-
+	// JavaScript로 해결
+	function solution(num1, num2) {
+		var answer = num1 / num2 * 1000;
+		
+		return Math.floor(answer);
+	}
 }
