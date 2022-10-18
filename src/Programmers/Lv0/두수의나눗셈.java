088c43fd6c7e7df6d4b5c19af2a1ca31ef373baf
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-18 15:41:04 +0900

    day21

--- src/Programmers/Lv0/두수의나눗셈.java
+++ src/Programmers/Lv0/두수의나눗셈.java
@@ -5,6 +5,6 @@ public class 두수의나눗셈 {
 	function solution(num1, num2) {
 		var answer = num1 / num2 * 1000;
 		
-		return Math.floor(answer);
+		return Math.floor(answer); // 해당 숫자 이하 가장 큰 정수 반환
 	}
 }
