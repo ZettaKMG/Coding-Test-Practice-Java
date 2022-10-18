a5c77d26c8c62cd797f49c0fe2f40cac76d21d66
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-18 16:50:55 +0900

    day21

--- src/Programmers/Lv0/배열두배만들기.java
+++ src/Programmers/Lv0/배열두배만들기.java
@@ -1,5 +1,15 @@
 package Programmers.Lv0;
 
 public class 배열두배만들기 {
-
+	class Solution {
+		public int[] solution(int[] numbers) {
+			
+			for (int i = 0; i < numbers.length; i++) {
+				numbers[i] = numbers[i] * 2;
+			}
+			
+			return numbers;
+			
+		}
+	}
 }
