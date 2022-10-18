cc40cc8fa43d168ee6f2cf5408e0b0b960839a89
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-18 12:33:35 +0900

    day21

--- src/Programmers/Lv0/숫자비교하기.java
+++ src/Programmers/Lv0/숫자비교하기.java
@@ -1,5 +1,15 @@
 package Programmers.Lv0;
 
 public class 숫자비교하기 {
-
+	class Solution {
+		public int solution(int num1, int num2) {
+			
+			if (num1 == num2) {
+				return 1;
+			} else {
+				return -1;
+			}
+			
+		}
+	}
 }
