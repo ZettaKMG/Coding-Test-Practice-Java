0ad90ba18952cea48b4e13d6c5f30e813cd2e57e
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-19 12:06:59 +0900

    day22(JavaScript)

--- src/Programmers/Lv0/최빈값구하기.java
+++ src/Programmers/Lv0/최빈값구하기.java
@@ -1,5 +1,18 @@
 package Programmers.Lv0;
 
-public class 최빈값구하기 {
+import java.lang.reflect.Array;
 
+public class 최빈값구하기 {
+	function solution(array) {
+		let arr = new Map();
+		
+		for (let i = 0; i < array.length; i++) {
+			arr.set(array[i], (arr.get(array[i]) || 0) + 1);
+		}
+		
+		let max = Math.max(.arr..[.arr..arr].map(v => v[1]));
+		let count = [...arr].filter(v => v[1] == max).length;
+		
+		return count > 1 ? -1 : [...arr].find(v => v[1] == max)[0];
+	}
 }
