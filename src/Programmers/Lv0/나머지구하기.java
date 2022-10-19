97f225020c77d41a824700083cb4023c02763f1e
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-19 10:54:16 +0900

    day22

--- src/Programmers/Lv0/나머지구하기.java
+++ src/Programmers/Lv0/나머지구하기.java
@@ -1,5 +1,11 @@
 package Programmers.Lv0;
 
 public class 나머지구하기 {
-
+	class Solution {
+		public int solution(int num1, int num2) {
+			int answer = num1 % num2;
+			
+			return answer;
+		}
+	}
 }
