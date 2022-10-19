532a1b91d93c5b134beb46513e582bf0896f8985
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-19 11:34:18 +0900

    day22

--- src/Programmers/Lv0/중앙값구하기.java
+++ src/Programmers/Lv0/중앙값구하기.java
@@ -1,5 +1,13 @@
 package Programmers.Lv0;
 
-public class 중앙값구하기 {
+import java.util.Arrays;
 
+public class 중앙값구하기 {
+	class Solution {
+		public int solution(int[] array) {
+			Arrays.sort(array);
+			
+			return array[array.length / 2];
+		}
+	}
 }
