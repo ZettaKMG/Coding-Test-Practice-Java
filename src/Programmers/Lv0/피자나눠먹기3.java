4c8c32af173880ecc118c63ee0263305264370a5
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-20 20:45:13 +0900

    day23

--- src/Programmers/Lv0/피자나눠먹기3.java
+++ src/Programmers/Lv0/피자나눠먹기3.java
@@ -1,5 +1,12 @@
 package Programmers.Lv0;
 
 public class 피자나눠먹기3 {
-
+	class Solution {
+		public int solution(int slice, int n) {
+			double count = Math.floor(n / slice); // 피자 판 수(몫)
+			
+			// 사람 수가 조각 수의 배수이면 몫 그대로, 아니면 몫 + 1 반환
+			return n % slice == 0 ? (int) count : (int) count + 1;
+		}
+	}
 }
