d6b666f7e64bd0f4e02f803d2f775ba005495c97
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-20 20:46:42 +0900

    day23

--- src/Programmers/Lv0/배열의평균값.java
+++ src/Programmers/Lv0/배열의평균값.java
@@ -1,5 +1,15 @@
 package Programmers.Lv0;
 
 public class 배열의평균값 {
-
+	class Solution {
+		public double solution(int[] numbers) {
+			double sum = 0;
+			
+			for (int i = 0; i < numbers.length; i++) {
+				sum += numbers[i];
+			}
+			
+			return sum / numbers.length;
+		}
+	}
 }
