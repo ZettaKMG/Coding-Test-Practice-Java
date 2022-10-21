ebc94ecfdb23ce1e260e9f6354f74b0acb67794a
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-21 20:24:08 +0900

    day24

--- src/Programmers/Lv0/배열뒤집기.java
+++ src/Programmers/Lv0/배열뒤집기.java
@@ -1,5 +1,16 @@
 package Programmers.Lv0;
 
 public class 배열뒤집기 {
-
+	class Solution {
+		public int[] solution(int[] num_list) {
+			int length = num_list.length;
+			int[] answer = new int[length];
+			
+			for (int i = 0; i < length; i++) {
+				answer[length - i - 1] = num_list[i];
+			}
+			
+			return answer;
+		}
+	}
 }
