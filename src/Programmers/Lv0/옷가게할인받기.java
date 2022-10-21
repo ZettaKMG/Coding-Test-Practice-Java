f0bedb83999880a5a662376bf7e654e3db41dc56
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-21 20:19:34 +0900

    day24(js)

--- src/Programmers/Lv0/옷가게할인받기.java
+++ src/Programmers/Lv0/옷가게할인받기.java
@@ -1,5 +1,17 @@
 package Programmers.Lv0;
 
 public class 옷가게할인받기 {
-
+	function solution(price) {
+		
+		if (price >= 100000 && price < 300000) {
+			return price * 0.95 << 0;
+		} else if (price >= 300000 && price < 500000) {
+			return price * 0.9 << 0;
+		} else if (price >= 500000) {
+			return price * 0.8 << 0;
+		} else {
+			return price;
+		}
+		
+	}
 }
