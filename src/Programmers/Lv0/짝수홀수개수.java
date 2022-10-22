b7bdc1a7648c02cccf1e67b923f8f1d41378dd8d
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-22 22:57:18 +0900

    day25

--- src/Programmers/Lv0/짝수홀수개수.java
+++ src/Programmers/Lv0/짝수홀수개수.java
@@ -1,5 +1,22 @@
 package Programmers.Lv0;
 
 public class 짝수홀수개수 {
-
+	class Solution {
+		public int[] solution(int[] num_list) {
+			int count_odd = 0;
+			int count_even = 0;
+			
+			for (int i = 0; i < num_list.length; i++) {
+				if (num_list[i] % 2 == 0) {
+					count_even++;
+				} else {
+					count_odd++;
+				}
+			}
+			
+			int[] answer = {count_even, count_odd};
+			
+			return answer;
+		}
+	}
 }
