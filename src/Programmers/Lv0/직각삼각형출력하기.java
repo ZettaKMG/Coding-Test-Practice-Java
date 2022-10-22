f0c83798843274fe52e7f340764cb32e6b244d55
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-22 22:37:43 +0900

    day25

--- src/Programmers/Lv0/직각삼각형출력하기.java
+++ src/Programmers/Lv0/직각삼각형출력하기.java
@@ -1,5 +1,21 @@
 package Programmers.Lv0;
 
-public class 직각삼각형출력하기 {
+import java.util.Scanner;
 
+public class 직각삼각형출력하기 {
+	public class Solution {
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			int n = sc.nextInt();
+			String s = "*";
+			
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j <= i; j++) {
+					System.out.print(s);
+				}
+				System.out.println();
+			}
+			
+		}
+	}
 }
