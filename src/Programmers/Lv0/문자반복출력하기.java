2a988ab9df6e9e14a1f3fd62f33712514cc1dde3
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-22 23:34:14 +0900

    day25

--- src/Programmers/Lv0/문자반복출력하기.java
+++ src/Programmers/Lv0/문자반복출력하기.java
@@ -1,5 +1,17 @@
 package Programmers.Lv0;
 
 public class 문자반복출력하기 {
-
+	class Solution {
+		public String solution(String my_string, int n) {
+			String answer = "";
+			
+			for (int i = 0; i < my_string.length(); i++) {
+				String re = my_string.substring(i, i + 1);
+				
+				answer += re.repeat(n);
+			}
+			
+			return answer;
+		}
+	}
 }
