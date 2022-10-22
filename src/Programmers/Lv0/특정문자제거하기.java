8d2f8e62689d93a8be257e2cf94e8a383d28f960
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-23 06:53:05 +0900

    day26

--- src/Programmers/Lv0/특정문자제거하기.java
+++ src/Programmers/Lv0/특정문자제거하기.java
@@ -1,5 +1,11 @@
 package Programmers.Lv0;
 
 public class 특정문자제거하기 {
-
+	class Solution {
+		public String solution(String my_string, String letter) {
+			String answer = my_string.replaceAll(letter, "");
+			
+			return answer;	
+		}
+	}
 }
