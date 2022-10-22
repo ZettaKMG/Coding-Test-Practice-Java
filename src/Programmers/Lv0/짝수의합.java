acc2b4ca5b0ebd67f91898bc3522abfa05cfa1ee
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-23 07:43:16 +0900

    day26

--- src/Programmers/Lv0/짝수의합.java
+++ src/Programmers/Lv0/짝수의합.java
@@ -1,5 +1,17 @@
 package Programmers.Lv0;
 
 public class 짝수의합 {
-
+	class Solution {
+		public int solution(int n) {
+			int even_sum = 0;
+			
+			for (int i = 0; i <= n; i++) {
+				if (i % 2 == 0) {
+					even_sum += i;
+				}
+			}
+			
+			return even_sum;
+		}
+	}
 }
