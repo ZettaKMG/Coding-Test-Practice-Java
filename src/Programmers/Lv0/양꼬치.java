9521d4354b23fb2453bb84c8411ad0742f49a448
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-23 16:49:36 +0900

    day26

--- src/Programmers/Lv0/양꼬치.java
+++ src/Programmers/Lv0/양꼬치.java
@@ -1,5 +1,14 @@
 package Programmers.Lv0;
 
 public class 양꼬치 {
-
+	class Solution {
+		public int solution(int n, int k) {
+			
+			if (n >= 10) {
+				k -= Math.floor(n / 10);				
+			}
+			
+			return 12000 * n + 2000 * k;			
+		}
+	}
 }
