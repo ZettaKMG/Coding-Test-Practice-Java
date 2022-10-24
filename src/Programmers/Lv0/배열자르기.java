0fd407ac679554b3527ad1786d85aa60f3d47178
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-24 10:26:41 +0900

    day27

--- src/Programmers/Lv0/배열자르기.java
+++ src/Programmers/Lv0/배열자르기.java
@@ -1,5 +1,13 @@
 package Programmers.Lv0;
 
-public class 배열자르기 {
+import java.util.Arrays;
 
+public class 배열자르기 {
+	class Solution {
+		public int[] solution(int[] numbers, int num1, int num2) {
+			int[] answer = Arrays.copyOfRange(numbers, num1, num2 + 1);
+			
+			return answer;
+		}
+	}
 }
