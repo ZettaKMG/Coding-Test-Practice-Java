e383843cf86ebdcfe0d47beb0f92d21e24de8ade
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-25 10:52:21 +0900

    day28 (js)

--- src/Programmers/Lv0/외계행성의나이.java
+++ src/Programmers/Lv0/외계행성의나이.java
@@ -1,5 +1,8 @@
 package Programmers.Lv0;
 
 public class 외계행성의나이 {
-
+	function solution(age) {
+		
+		return [...('' + age)].map(num => String.fromCharCode('a'.charCodeAt(0) + + num)).join('');
+	}
 }
