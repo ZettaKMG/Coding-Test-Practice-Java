214f3ddfa50b57c6cfd6dcd23b295d5e477ee43d
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-25 10:54:08 +0900

    day28

--- src/Programmers/Lv0/순서쌍의개수.java
+++ src/Programmers/Lv0/순서쌍의개수.java
@@ -1,5 +1,21 @@
 package Programmers.Lv0;
 
 public class 순서쌍의개수 {
-
+	class Solution {
+		public int solution(int n) {
+			int count = 0;
+			
+			for (int i = 1; i < Math.sqrt(n); i++) {
+				if (n % i == 0) {
+					count += 2;
+				}
+			}
+			
+			if (n % Math.sqrt(n) == 0) {
+				count++;
+			}
+			
+			return count;
+		}
+	}
 }
