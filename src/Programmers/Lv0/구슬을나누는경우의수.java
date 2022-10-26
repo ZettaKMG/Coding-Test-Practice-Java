2600f72ffb534b0afbf549b86f8d34d46a9db1dc
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-26 18:21:03 +0900

    day29

--- src/Programmers/Lv0/구슬을나누는경우의수.java
+++ src/Programmers/Lv0/구슬을나누는경우의수.java
@@ -1,5 +1,24 @@
 package Programmers.Lv0;
 
-public class 구슬을나누는경우의수 {
+import java.math.BigInteger;
 
+public class 구슬을나누는경우의수 {
+	class Solution {
+		public BigInteger solution(int balls, int share) {
+			BigInteger n = BigInteger.ONE;
+			BigInteger m = BigInteger.ONE;
+			long choose1 = balls - share > share ? balls - share : share;
+			long choose2 = balls - share > share ? share : balls - share;
+			
+			for (long i = choose1 + 1; i <= balls; i++) {
+				n = n.multiply(BigInteger.valueOf(i));
+			}
+			
+			for (long j = 2L; j <= choose2; j++) {
+				m = m.multiply(BigInteger.valueOf(j));
+			}
+			
+			return n.divide(m);
+		}
+	}
 }
