99e0cf600b63e36bca985698c4bf9ebcfce501a6
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-26 18:31:21 +0900

    day29

--- src/Programmers/Lv0/개미군단.java
+++ src/Programmers/Lv0/개미군단.java
@@ -1,5 +1,14 @@
 package Programmers.Lv0;
 
 public class 개미군단 {
-
+	class Solution {
+		public int solution(int hp) {
+			double general = Math.floor(hp / 5);
+			double soldier = Math.floor((hp - general * 5) / 3);
+			double worker = hp - general * 5 - soldier * 3;
+			double answer = general + soldier + worker;
+			
+			return (int) answer;
+		}
+	}
 }
