5e9dd4513c5b2b5d934293651d7c90ae39871e85
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-26 23:52:52 +0900

    day29 (js)

--- src/Programmers/Lv0/가위바위보.java
+++ src/Programmers/Lv0/가위바위보.java
@@ -1,5 +1,9 @@
 package Programmers.Lv0;
 
 public class 가위바위보 {
-
+	function solution(rsp) {
+		const rsp_win = {0 : 5, 2 : 0, 5 : 2};
+		
+		return [...rsp].map(num => rsp_win[num]).join('');
+	}
 }
