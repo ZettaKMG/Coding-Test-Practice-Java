2427b3b030ff9c1593dfd0da19f4d860fe023a27
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-27 09:48:16 +0900

    day30

--- src/Programmers/Lv0/점의위치구하기.java
+++ src/Programmers/Lv0/점의위치구하기.java
@@ -1,5 +1,17 @@
 package Programmers.Lv0;
 
 public class 점의위치구하기 {
-
+	class Solution {
+		public int solution(int[] dot) {
+			if (dot[0] > 0 && dot[1] > 0) {
+				return 1;
+			} else if (dot[0] < 0 && dot[1] > 0) {
+				return 2;
+			} else if (dot[0] < 0 && dot[1] < 0) {
+				return 3;
+			} else {
+				return 4;
+			}
+		}
+	}
 }
