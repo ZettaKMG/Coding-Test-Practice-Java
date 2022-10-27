5332e1127be183c455813d8acd18cfa75412d1b8
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-27 11:06:36 +0900

    day30

--- src/Programmers/Lv0/이차원으로만들기.java
+++ src/Programmers/Lv0/이차원으로만들기.java
@@ -1,5 +1,13 @@
 package Programmers.Lv0;
 
 public class 이차원으로만들기 {
-
+	function solution(num_list, n) {
+		let answer = [];
+		
+		for (let i = 0; i < num_list.length / n; i++) {
+			answer = [...answer, num_list.slice(i * n, i * n + n)];			
+		}
+		
+		return answer;
+	}
 }
