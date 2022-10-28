a364b6b0d2ee5b36e179e42a5547b821ca5dbdd4
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-28 21:37:55 +0900

    day31

--- src/Programmers/Lv0/주사위의개수.java
+++ src/Programmers/Lv0/주사위의개수.java
@@ -1,5 +1,13 @@
 package Programmers.Lv0;
 
 public class 주사위의개수 {
-
+	class Solution {
+		public int solution(int[] box, int n) {
+			double width = Math.floor(box[0] / n);
+			double vertical = Math.floor(box[1] / n);
+			double height = Math.floor(box[2] / n);
+			
+			return (int) width * (int) vertical * (int) height;
+		}
+	}
 }
