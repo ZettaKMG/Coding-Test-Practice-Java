cd4661326c23ee39ac3f2b54fd958891e046a325
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-29 11:43:52 +0900

    day32

--- src/Programmers/Lv0/최댓값만들기1.java
+++ src/Programmers/Lv0/최댓값만들기1.java
@@ -1,5 +1,16 @@
 package Programmers.Lv0;
 
-public class 최댓값만들기1 {
+import java.util.Arrays;
 
+public class 최댓값만들기1 {
+	class Solution {
+		public int solution(int[] numbers) {
+			Arrays.sort(numbers);
+			
+			int max1 = numbers[numbers.length - 1];
+			int max2 = numbers[numbers.length - 2];
+			
+			return max1 * max2;
+		}
+	}
 }
