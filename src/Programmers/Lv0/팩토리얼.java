1aa4c968ec8335affe815e86997574229013e143
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-29 11:45:27 +0900

    day32

--- src/Programmers/Lv0/팩토리얼.java
+++ src/Programmers/Lv0/팩토리얼.java
@@ -1,5 +1,21 @@
 package Programmers.Lv0;
 
 public class 팩토리얼 {
-
+	class Solution {
+		public int solution(int n) {
+			int factorial = 1;
+			int i = 0;
+			
+			while (true) {
+				if (factorial <= n) {
+					factorial *= i + 1;
+					i++;
+				} else {
+					break;
+				}
+			}
+			
+			return i - 1;
+		}
+	}
 }
