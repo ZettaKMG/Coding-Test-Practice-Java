42a5f764d23212887927278140ee1e75c7be8fde
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-31 07:07:35 +0900

    day34

--- src/Programmers/Lv0/중복된문자제거.java
+++ src/Programmers/Lv0/중복된문자제거.java
@@ -1,5 +1,17 @@
 package Programmers.Lv0;
 
 public class 중복된문자제거 {
-
+	class Solution {
+		public String solution(String my_string) {
+			String result = "";
+			
+			for (int i = 0; i < my_string.length(); i++) {
+				if (i == my_string.indexOf(my_string.charAt(i))) {
+					result += my_string.charAt(i);
+				}
+			}
+			
+			return result;
+		}
+	}
 }
