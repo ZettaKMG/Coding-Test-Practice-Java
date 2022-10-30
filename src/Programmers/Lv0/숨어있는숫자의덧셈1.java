17f936a259051bcbaa1efa8dcbaa11f36de6a508
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-30 18:09:27 +0900

    day33

--- src/Programmers/Lv0/숨어있는숫자의덧셈1.java
+++ src/Programmers/Lv0/숨어있는숫자의덧셈1.java
@@ -1,5 +1,17 @@
 package Programmers.Lv0;
 
 public class 숨어있는숫자의덧셈1 {
-
+	class Solution {
+		public int solution(String my_string) {
+			int result = 0;
+			
+			for (int i = 0; i < my_string.length(); i++) {
+				if (my_string.charAt(i) > 48 && my_string.charAt(i) < 58) {
+					result += Integer.parseInt("" + my_string.charAt(i));
+				}
+			}
+			
+			return result;
+		}
+	}
 }
