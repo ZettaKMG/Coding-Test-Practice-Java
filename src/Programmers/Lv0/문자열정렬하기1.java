d89e3b48849fdab6790916be0f8f54326ab84d8b
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-30 18:05:07 +0900

    day33

--- src/Programmers/Lv0/문자열정렬하기1.java
+++ src/Programmers/Lv0/문자열정렬하기1.java
@@ -1,5 +1,20 @@
 package Programmers.Lv0;
 
-public class 문자열정렬하기1 {
+import java.util.Arrays;
 
+public class 문자열정렬하기1 {
+	class Solution {
+		public int[] solution(String my_string) {
+			my_string = my_string.replaceAll("[a-z]", "");
+			int[] result = new int[my_string.length()];
+			
+			for (int i = 0; i < my_string.length(); i++) {
+				result[i] = my_string.charAt(i) - '0';
+			}
+			
+			Arrays.sort(result);
+			
+			return result;
+		}
+	}
 }
