695aa0df764d7eef9889363d52142ffddf899ea1
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-31 06:59:43 +0900

    day34

--- src/Programmers/Lv0/삼각형의완성조건1.java
+++ src/Programmers/Lv0/삼각형의완성조건1.java
@@ -1,5 +1,17 @@
 package Programmers.Lv0;
 
 public class 삼각형의완성조건1 {
-
+	class Solution {
+		public int solution(int[] sides) {
+			if (sides[0] + sides[1] <= sides[2]) {
+				return 2;
+			} else if (sides[0] + sides[2] <= sides[1]) {
+				return 2;
+			} else if (sides[1] + sides[2] <= sides[0]) {
+				return 2;
+			} else {
+				return 1;
+			}
+		}
+	}
 }
