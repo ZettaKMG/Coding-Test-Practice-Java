41d5008e4119e54f8f715166176ccbcbb8ae3f85
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-30 18:02:49 +0900

    day33

--- src/Programmers/Lv0/모음제거.java
+++ src/Programmers/Lv0/모음제거.java
@@ -1,5 +1,13 @@
 package Programmers.Lv0;
 
 public class 모음제거 {
-
+	class Solution {
+		public String solution(String my_string) {
+			String result ="";
+			
+			result = my_string.replaceAll("[aeiou]", "");
+			
+			return result;
+		}
+	}
 }
