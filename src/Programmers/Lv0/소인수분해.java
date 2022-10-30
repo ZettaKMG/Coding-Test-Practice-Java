b842bd532b87c22b05f913fa74eca083d7a46be0
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-30 18:07:39 +0900

    day33

--- src/Programmers/Lv0/소인수분해.java
+++ src/Programmers/Lv0/소인수분해.java
@@ -1,5 +1,27 @@
 package Programmers.Lv0;
 
-public class 소인수분해 {
+import java.util.ArrayList;
+import java.util.List;
 
+public class 소인수분해 {
+	class Solution {
+		public int[] solution(int n) {
+			List<Integer> factorization_number = new ArrayList<>();
+			
+			for (int i = 2; i <= n; i++) {
+				if (n % i == 0) {
+					factorization_number.add(i);
+					
+					while (n % i == 0) {
+						n /= i;
+					}
+					
+				}
+			}
+			
+			int[] result = factorization_number.stream().mapToInt(i -> i).toArray();
+			
+			return result;
+		}
+	}
 }
