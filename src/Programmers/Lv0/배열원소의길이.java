136d149242c54e3b34c7b00befe0393be703f162
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-31 07:36:13 +0900

    day34

--- src/Programmers/Lv0/배열원소의길이.java
+++ src/Programmers/Lv0/배열원소의길이.java
@@ -1,5 +1,17 @@
 package Programmers.Lv0;
 
-public class 배열원소의길이 {
+import java.util.ArrayList;
 
+public class 배열원소의길이 {
+	class Solution {
+		public ArrayList solution(String[] strlist) {
+			ArrayList<Integer> result = new ArrayList<>();
+			
+			for (String str : strlist) {
+				result.add(str.length());
+			}
+			
+			return result;
+		}
+	}
 }
