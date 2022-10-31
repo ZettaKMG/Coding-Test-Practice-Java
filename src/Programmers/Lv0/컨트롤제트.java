9bb48c665c65e95e68dc1b4364974acb2e9569d0
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-10-31 22:05:51 +0900

    day34

--- src/Programmers/Lv0/컨트롤제트.java
+++ src/Programmers/Lv0/컨트롤제트.java
@@ -1,5 +1,29 @@
 package Programmers.Lv0;
 
-public class 컨트롤제트 {
+import java.util.ArrayList;
 
+public class 컨트롤제트 {
+	class Solution {
+		public int solution(String s) {
+			int result = 0;
+			String[] str = s.split(" ");
+			ArrayList<Integer> numbers = new ArrayList<>();
+			
+			for (int i = 0; i < str.length; i++) {
+				if (str[i].equals("Z")) {
+					if (numbers.size() != 0) {
+						numbers.remove(numbers.size() - 1);
+					}
+				} else {
+					numbers.add(Integer.parseInt(str[i]));
+				}
+			}
+			
+			for (int number : numbers) {
+				result += number;
+			}
+			
+			return result;
+		}
+	}
 }
