ef89e780fd42adffa9e1463fbd4650159714ef36
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-01 22:31:52 +0900

    day35

--- src/Programmers/Lv0/대문자와소문자.java
+++ src/Programmers/Lv0/대문자와소문자.java
@@ -1,5 +1,21 @@
 package Programmers.Lv0;
 
 public class 대문자와소문자 {
-
+	class Solution {
+		public String solution(String my_string) {
+			String result = "";
+			
+			for (int i = 0; i < my_string.length(); i++) {
+				char c = my_string.charAt(i);
+				
+				if (Character.isLowerCase(c)) {
+					result += String.valueOf(c).toUpperCase();
+				} else {
+					result += String.valueOf(c).toLowerCase();
+				}
+			}
+			
+			return result;
+		}
+	}
 }
