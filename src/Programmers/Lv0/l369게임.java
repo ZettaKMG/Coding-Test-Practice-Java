0cbc143afa87eaf0f6b86a3cdf7019d92508a7a1
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-01 22:27:32 +0900

    day35

--- src/Programmers/Lv0/l369게임.java
+++ src/Programmers/Lv0/l369게임.java
@@ -1,5 +1,22 @@
 package Programmers.Lv0;
 
 public class l369게임 {
-
+	class Solution {
+		public int solution(int order) {
+			String order_str = Integer.toString(order);
+			int count = 0;
+			
+			for (int i = 0; i < order_str.length(); i++) {
+				if (order_str.charAt(i) == '3') {
+					count++;
+				} else if (order_str.charAt(i) == '6') {
+					count++;
+				} else if (order_str.charAt(i) == '9') {
+					count++;
+				}
+			}
+			
+			return count;
+		}
+	}
 }
