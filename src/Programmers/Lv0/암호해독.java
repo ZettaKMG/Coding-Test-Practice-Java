9a0966c2d49094e77e7d0394ce9b4d29bc7cc857
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-01 22:29:05 +0900

    day35

--- src/Programmers/Lv0/암호해독.java
+++ src/Programmers/Lv0/암호해독.java
@@ -1,5 +1,17 @@
 package Programmers.Lv0;
 
 public class 암호해독 {
-
+	class Solution {
+		public String solution(String cipher, int code) {
+			String result = "";
+			
+			for (int i = 0; i < cipher.length(); i++) {
+				if (i % code == (code - 1)) {
+					result += cipher.substring(i, i + 1); 
+				}
+			}
+			
+			return result;
+		}
+	}
 }
