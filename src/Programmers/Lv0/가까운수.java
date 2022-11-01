3b27c7020637625b9e747febce169165dc295af5
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-01 22:24:02 +0900

    day35

--- src/Programmers/Lv0/가까운수.java
+++ src/Programmers/Lv0/가까운수.java
@@ -1,5 +1,22 @@
 package Programmers.Lv0;
 
-public class 가까운수 {
+import java.util.Arrays;
 
+public class 가까운수 {
+	class Solution {
+		public int solution(int[] array, int n) {
+			int result = 0;
+			Arrays.sort(array);
+			
+			for (int i = 0; i < array.length; i++) {
+				if (Math.abs(array[0] - n) > Math.abs(array[i] - n)) {
+					array[0] = array[i];
+				}
+				
+				result = array[0];
+			}
+			
+			return result;
+		}
+	}
 }
