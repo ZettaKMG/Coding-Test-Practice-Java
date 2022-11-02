9f7ddb57ab9750b6efcfe0092a82ad790ada0cd2
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-02 20:33:50 +0900

    day36

--- src/Programmers/Lv0/약수구하기.java
+++ src/Programmers/Lv0/약수구하기.java
@@ -1,5 +1,25 @@
 package Programmers.Lv0;
 
 public class 약수구하기 {
+	class Solution {
+	    public int[] solution(int n) {
+	        int[] array = {};
+	        array = new int[n];
+	        int div = 0;
+	        
+	        for (int i = 1; i <= n; i++) {
+	            if(n % i == 0) {
+	                array[div++] = i;
+	            }
+	        }
+	        
+	        int[] result = new int[div];
+	        
+	        for (int i = 0; i < div; i++){
+	            result[i] = array[i];
+	        }
 
+	        return result;
+	    }
+	}
 }
