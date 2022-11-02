0238667b29063a4441ee6ff90444e28696bef7a4
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-02 20:24:07 +0900

    day36

--- src/Programmers/Lv0/인덱스바꾸기.java
+++ src/Programmers/Lv0/인덱스바꾸기.java
@@ -1,5 +1,19 @@
 package Programmers.Lv0;
 
 public class 인덱스바꾸기 {
-
+	class Solution {
+	    public String solution(String my_string, int num1, int num2) {
+	        String result = "";
+	        String[] array = my_string.split("");
+	        String temp = array[num1];        
+	        array[num1] = array[num2];
+	        array[num2] = temp;
+	        
+	        for(String str : array){
+	            result += str;
+	        }
+	        
+	        return result;
+	    }
+	}
 }
