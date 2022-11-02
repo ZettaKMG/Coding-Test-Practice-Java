0b4a4db2ec117a9a14a28b5aa8ddcd2c8fbb1eff
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-02 20:28:13 +0900

    day36

--- src/Programmers/Lv0/한번만등장한문자.java
+++ src/Programmers/Lv0/한번만등장한문자.java
@@ -1,5 +1,27 @@
 package Programmers.Lv0;
 
-public class 한번만등장한문자 {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 
+public class 한번만등장한문자 {
+	class Solution {
+	    public String solution(String s) {
+
+	        HashSet<String> set = new HashSet<>();
+
+	        for (int i = 0; i < s.length(); i++) {
+	            String replace = s.replace(s.charAt(i) + "", "");
+	            
+	            if(s.length() - replace.length() == 1){
+	                set.add(s.charAt(i)+"");
+	            }            
+	        }
+	        
+	        ArrayList<String> list = new ArrayList<>(set);
+	        Collections.sort(list);
+	        
+	        return String.join("", list);
+	    }
+	}
 }
