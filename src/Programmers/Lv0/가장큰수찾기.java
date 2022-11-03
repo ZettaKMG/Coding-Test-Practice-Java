8bbbf6bf4f54138ad799960deb16c7fa29634090
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-03 17:54:36 +0900

    day37

--- src/Programmers/Lv0/가장큰수찾기.java
+++ src/Programmers/Lv0/가장큰수찾기.java
@@ -1,5 +1,21 @@
 package Programmers.Lv0;
 
 public class 가장큰수찾기 {
-
+	class Solution {
+		public int[] solution(int[] array) {
+			int max = 0;
+			int index = 0;
+			
+			for (int i = 0; i < array.length; i++) {
+				if (max < array[i]) {
+					max = array[i];
+					index = i;
+				}
+			}
+			
+			int[] result = {max, index};
+			
+			return result;
+		}
+	}
 }
