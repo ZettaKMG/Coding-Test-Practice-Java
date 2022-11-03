10db539959460e4a38bb0939a95feb34339bf303
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-03 17:59:20 +0900

    day37

--- src/Programmers/Lv0/배열의유사도.java
+++ src/Programmers/Lv0/배열의유사도.java
@@ -1,5 +1,19 @@
 package Programmers.Lv0;
 
 public class 배열의유사도 {
-
+	class Solution {
+		public int solution(String[] s1, String[] s2) {
+			int count = 0;
+			
+			for (int i = 0; i < s1.length; i++) {
+				for (int j = 0; j < s2.length; j++) {
+					if (s1[i].equals(s2[j])) {
+						count++;
+					}
+				}
+			}
+			
+			return count;
+		}
+	}
 }
