6a0cd413ffe01ca33087b55adaeb38b022e4e6d6
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-03 17:57:35 +0900

    day37

--- src/Programmers/Lv0/문자열계산하기.java
+++ src/Programmers/Lv0/문자열계산하기.java
@@ -1,5 +1,20 @@
 package Programmers.Lv0;
 
 public class 문자열계산하기 {
-
+	class Solution {
+		public int solution(String my_string) {
+			String[] str_array = my_string.split(" ");
+			int result = Integer.parseInt(str_array[0]);
+			
+			for (int i = 1; i < str_array.length; i+=2) {
+				if (str_array[i].equals('+')) {
+					result += Integer.parseInt(str_array[i + 1]);
+				} else if (str_array[i].equals("-")) {
+					result += Integer.parseInt(str_array[i + 1]);
+				}
+			}
+			
+			return result;
+		}
+	}
 }
