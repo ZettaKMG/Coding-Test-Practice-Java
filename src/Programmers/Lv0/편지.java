cafec9c8675ca36a5d9665dc41f634e31be07e0f
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-03 18:00:20 +0900

    day37

--- src/Programmers/Lv0/편지.java
+++ src/Programmers/Lv0/편지.java
@@ -1,5 +1,15 @@
 package Programmers.Lv0;
 
 public class 편지 {
-
+	class Solution {
+		public int solution(String message) {
+			int count = 0;
+			
+			for (int i = 0; i < message.length(); i++) {
+				count++;
+			}
+			
+			return count * 2;
+		}
+	}
 }
