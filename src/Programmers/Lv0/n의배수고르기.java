71e33d7ade9f17395f4efdf16dc885acde69cbbe
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-06 20:46:41 +0900

    day38

--- src/Programmers/Lv0/n의배수고르기.java
+++ src/Programmers/Lv0/n의배수고르기.java
@@ -1,5 +1,25 @@
 package Programmers.Lv0;
 
-public class n의배수고르기 {
+import java.util.ArrayList;
 
+public class n의배수고르기 {
+	class Solution {
+	    public int[] solution(int n, int[] numlist) {
+	        ArrayList<Integer> List = new ArrayList<>();
+	        
+	        for (int i = 0;i < numlist.length; i++) {
+	            if (numlist[i] % n == 0) {
+	                List.add(numlist[i]);
+	            }        
+	        }    
+	        
+	        int[] result = new int[List.size()];
+	        
+	        for (int i = 0; i< List.size(); i++) {
+	            result[i] = List.get(i);
+	        }
+	        
+	        return result;
+	    }
+	}
 }
