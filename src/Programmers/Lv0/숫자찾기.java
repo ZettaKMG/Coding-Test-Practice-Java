f3ac83832aee8860e25d8b7b2dca0d6ca64893b0
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-06 20:47:29 +0900

    day38

--- src/Programmers/Lv0/숫자찾기.java
+++ src/Programmers/Lv0/숫자찾기.java
@@ -1,5 +1,13 @@
 package Programmers.Lv0;
 
 public class 숫자찾기 {
-
+	class Solution {
+	    public int solution(int num, int k) {
+	        String num_str = String.valueOf(num);
+	        String k_str = String.valueOf(k);
+	        int result = num_str.indexOf(k_str);
+	        
+	        return result >= 0 ? (result + 1) : -1;
+	    }
+	}
 }
