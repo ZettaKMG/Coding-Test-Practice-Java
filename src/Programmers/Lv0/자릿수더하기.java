3829add30461c3f89b359a26f14cfca28979e89b
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-06 20:47:49 +0900

    day38

--- src/Programmers/Lv0/자릿수더하기.java
+++ src/Programmers/Lv0/자릿수더하기.java
@@ -1,5 +1,17 @@
 package Programmers.Lv0;
 
 public class 자릿수더하기 {
+	class Solution {
+	    public int solution(int n) {
+	        int result = 0;
+	        String str = Integer.toString(n);
+	        String[] array = str.split("");
 
+	        for(int i = 0; i < array.length; i++) {
+	            result += Integer.parseInt(array[i]);
+	        }
+	        
+	        return result;
+	    }
+	}
 }
