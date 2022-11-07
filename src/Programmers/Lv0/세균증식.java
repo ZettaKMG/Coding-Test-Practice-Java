dc17fac63583a749418e3bf5793774ec6592b49f
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-07 16:38:01 +0900

    day39

--- src/Programmers/Lv0/세균증식.java
+++ src/Programmers/Lv0/세균증식.java
@@ -1,5 +1,11 @@
 package Programmers.Lv0;
 
 public class 세균증식 {
-
+	class Solution {
+		public int solution(int n, int t) {
+			double result = n * Math.pow(2, t);
+			
+			return (int) result;
+		}
+	}
 }
