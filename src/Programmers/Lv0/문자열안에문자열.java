d68caabf106b799f2f1b587d1b6bbeb713afffa8
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-07 16:39:00 +0900

    day39

--- src/Programmers/Lv0/문자열안에문자열.java
+++ src/Programmers/Lv0/문자열안에문자열.java
@@ -1,5 +1,13 @@
 package Programmers.Lv0;
 
 public class 문자열안에문자열 {
-
+	class Solution {
+		public int solution(String str1, String str2) {
+			if (str1.indexOf(str2) >= 0) {
+				return 1;
+			} else {
+				return 2;
+			}
+		}
+	}
 }
