50162977af474c65cdac255c3de5be5dce1556c0
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-07 16:37:02 +0900

    day39

--- src/Programmers/Lv0/제곱수판별하기.java
+++ src/Programmers/Lv0/제곱수판별하기.java
@@ -1,5 +1,15 @@
 package Programmers.Lv0;
 
 public class 제곱수판별하기 {
-
+	class Solution {
+		public int solution(int n) {
+			for (int i = 1; i < n; i++) {
+				if (n == Math.pow(i, 2)) {
+					return 1;
+				}
+			}
+			
+			return 2;
+		}
+	}
 }
