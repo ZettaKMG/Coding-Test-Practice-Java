94784239c555957a0f4c6f272b28bcbb9e05fd03
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-07 17:25:25 +0900

    day39

--- src/Programmers/Lv0/문자열정렬하기2.java
+++ src/Programmers/Lv0/문자열정렬하기2.java
@@ -1,5 +1,17 @@
 package Programmers.Lv0;
 
-public class 문자열정렬하기2 {
+import java.util.Arrays;
 
+public class 문자열정렬하기2 {
+	class Solution {
+		public String solution(String my_string) {
+			char[] str_char = my_string.toLowerCase().toCharArray();
+			
+			Arrays.sort(str_char);
+			
+			String result = new String(str_char);
+			
+			return result;
+		}
+	}
 }
