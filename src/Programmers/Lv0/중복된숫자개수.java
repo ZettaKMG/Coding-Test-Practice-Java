2a409172cbfaa4e06c189b273f934881cca114b5
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-15 22:52:00 +0900

    day40

--- src/Programmers/Lv0/중복된숫자개수.java
+++ src/Programmers/Lv0/중복된숫자개수.java
@@ -1,5 +1,18 @@
 package Programmers.Lv0;
 
 public class 중복된숫자개수 {
-
+	class Solution {
+	    public int solution(int[] array, int n) {
+	        int count = 0; // n의 개수
+	        
+	        // 배열의 각 숫자가 n과 일치할 때마다 count 더하기
+	        for (int i = 0; i < array.length; i++) {
+	            if (array[i] == n) {
+	                count++;
+	            }
+	        }
+	        
+	        return count; // 결과 반환
+	    }
+	}
 }
