521c9a13edcbfcf1f40a29dcdd8745d7e2d684a7
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-23 17:18:55 +0900

    day47

--- src/Programmers/Lv0/종이자르기.java
+++ src/Programmers/Lv0/종이자르기.java
@@ -1,5 +1,11 @@
 package Programmers.Lv0;
 
 public class 종이자르기 {
-
+	class Solution {
+	    public int solution(int M, int N) {
+	        int min_count = M * N - 1; // 테스트 케이스를 보고 규칙성이 보여서 공식화
+	        
+	        return min_count; // 결과 반환
+	    }
+	}
 }
